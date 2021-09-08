122bbf0d14060cf730f1e908b53c7476ccf7fd6b
Anthony Briglia <ambriglia@gmail.com>
2021-09-08 10:13:02 -0400

    concurrently

--- 19/server.js
+++ 19/server.js
@@ -5,8 +5,4 @@ const app = express();
 
 const PORT = 3001;
 
-app.get('/api/products', (req, res) => {
-  return res.json(products);
-});
-
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
