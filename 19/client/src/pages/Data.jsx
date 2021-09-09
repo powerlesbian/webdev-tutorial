c280135e51ae5d65e6bda5aa06a08b3bd9636272
Anthony Briglia <ambriglia@gmail.com>
2021-09-08 20:59:43 -0400

    remove users from global state

--- 19/client/src/pages/Data.jsx
+++ 19/client/src/pages/Data.jsx
@@ -1,12 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 import { useBankContext } from "../utils/BankContext";
 
 const Data = () => {
+  const [users, setUsers] = useState([]);
+
   const { bank } = useBankContext();
 
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get('/api/users');
+      setUsers(res.data);
+    }
+    getData();
+  }, []);
+
   return (
-    <div>
-      {JSON.stringify(bank, null, 2)}
-    </div>
+    <>
+      <p>Global state</p>
+      <div>
+        {JSON.stringify(bank, null, 2)}
+      </div>
+      <p>Users</p>
+      <div>
+        {JSON.stringify(users, null, 2)}
+      </div>
+    </>
   )
 };
 
--- 19/client/src/utils/BankContext.jsx
+++ 19/client/src/utils/BankContext.jsx
@@ -2,10 +2,7 @@ import {
   createContext,
   useContext,
   useState,
-  useEffect,
 } from "react";
-
-import axios from 'axios';
 
 const BankContext = createContext();
 
@@ -14,19 +11,7 @@ export const useBankContext = () => useContext(BankContext);
 export const BankProvider = ({ children }) => {
   const [bank, setBank] = useState({
     loggedInUser: null,
-    users: [],
   });
-
-  useEffect(() => {
-    const getData = async () => {
-      const res = await axios.get('/api/users');
-      setBank({
-        ...bank,
-        users: res.data,
-      });
-    }
-    getData();
-  }, []);
 
   const setLoggedInUser = (username) => {
     setBank({
@@ -35,17 +20,9 @@ export const BankProvider = ({ children }) => {
     });
   }
 
-  const addUser = (user) => {
-    setBank({
-      ...bank,
-      users: [...bank.users, user]
-    });
-  }
-
   return (
     <BankContext.Provider value={{
       bank,
-      addUser,
       setLoggedInUser,
     }}>
       {children}
